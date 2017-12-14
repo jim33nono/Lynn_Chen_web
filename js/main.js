a9748ac9b93a97103a695892a046044b3e8dbf62
TW\lynnchen <jim33nono@gmail.com>
2017-12-14 11:55:32 +0800

    initial

---
+++ js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    //    $.get("https://api.myjson.com/bins/1afts3", function (data, status) {
+    //        alert(`Data: ${JSON.stringify(data)}\nStatus: ${status}`);
+    //    });
+
+
+
+    $("#submitMessage").click(function () {
+        var inputEmail = $('#inputEmail1').val();
+        var userName = $('#userName').val();
+        var messageTextarea = $('#messageTextarea').val();
+
+        console.log(`${inputEmail} ${userName} ${messageTextarea}`);
+        if (inputEmail == '' || userName == '' || messageTextarea == '') {
+            alert('information must be fill up!');
+            return false;
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+            data: {
+                'key': 'E07DUkbmRAAJKU7zJ7TFnA',
+                'message': {
+                    'from_email': 'jim33nono@gmail.com',
+                    'to': [{
+                        'email': $('#inputEmail1').val(),
+                        'name': $('#userName').val(),
+                        'type': 'to'
+                                              }],
+                    'autotext': 'true',
+                    'subject': 'Customer Message',
+                    'html': $('#messageTextarea').val()
+                }
+            }
+        }).done(function (response) {
+            console.log($('#inputEmail1').val());
+            console.log($('#userName').val());
+            console.log($('#messageTextarea').val());
+            console.log(response);
+            alert('Emial be sent')
+        }).fail(function (response) {
+            alert("System error");
+
+
+
+
+        });
+
+    });
+
+    var navigation = $('nav');
+    navigation.on('click', '.option', function (event) {
+        topIconFunction();
+
+    });
+});
+
+function topIconFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+
+}
